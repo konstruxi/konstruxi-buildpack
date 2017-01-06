f638c964ee70187c57549f1f1e1ba2a74018568f
Yaroslaff Fedin <invizko@gmail.com>
2017-01-06 20:22:25 +0800

    Tweak script

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -21,6 +21,11 @@ python -m SimpleHTTPServer $PORT &
 cd $temp_dir
 echo "Temp dir: $temp_dir"
 
+PCRE_VERSION=${PCRE_VERSION-8.21}
+pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
+
+echo "Downloading $pcre_tarball_url"
+(curl -L $pcre_tarball_url | tar xvj )
 
 # Main dependencies
 git clone https://github.com/nginx/nginx --depth=1;
@@ -37,8 +42,50 @@ git clone https://github.com/vkholodkov/nginx-upload-module.git --depth=1;
 git clone https://github.com/masterzen/nginx-upload-progress-module.git --depth=1;
 
 # Compile nginx (change path to your app)
-env APP_PATH=./skema ./skema/compile-nginx.sh
+env APP_PATH=.;
 
+if [ -z "$APP_PATH" ]; then APP_PATH="../skema"; fi
+
+cd nginx;
+
+env CFLAGS="-Wno-error" ./configure \
+  --prefix=$APP_PATH/conf \
+  --with-http_ssl_module \
+  --with-pcre=../pcre-${PCRE_VERSION} \
+  --with-ipv6 \
+  --sbin-path=../bin/nginx \
+  --conf-path=nginx.conf \
+  --pid-path=../temp/run/nginx.pid \
+  --lock-path=../temp/run/nginx.lock \
+  --http-client-body-temp-path=../temp/client_body_temp \
+  --http-proxy-temp-path=../temp/proxy_temp \
+  --http-fastcgi-temp-path=../temp/fastcgi_temp \
+  --http-uwsgi-temp-path=../temp/uwsgi_temp \
+  --http-scgi-temp-path=../temp/scgi_temp \
+  --http-log-path=../logs/access.log \
+  --error-log-path=../logs/error.log \
+  --with-debug \
+  --add-module=../ngx_devel_kit \
+  --add-module=../set-misc-nginx-module \
+  --add-module=../form-input-nginx-module \
+  --add-module=../ngx_postgres \
+  --add-module=../mustache-nginx-module \
+  --add-module=../echo-nginx-module \
+  --add-module=../nginx-eval-module \
+  --add-module=../ngx_coolkit;
+make install;
+rm -rf $APP_PATH/conf/fastcgi.conf;
+rm -rf $APP_PATH/conf/fastcgi.conf.default;
+rm -rf $APP_PATH/conf/fastcgi_params;
+rm -rf $APP_PATH/conf/fastcgi_params.default;
+rm -rf $APP_PATH/conf/koi-utf;
+rm -rf $APP_PATH/conf/koi-win;
+rm -rf $APP_PATH/conf/win-utf;
+rm -rf $APP_PATH/conf/scgi_params;
+rm -rf $APP_PATH/conf/scgi_params.default;
+rm -rf $APP_PATH/conf/uwsgi_params;
+rm -rf $APP_PATH/conf/uwsgi_params.default;
+rm -rf $APP_PATH/conf/html;
 
 while true
 do
