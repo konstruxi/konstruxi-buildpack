42b3c8fd550acc65aff4981d5783262ec77f6426
Yaroslaff Fedin <invizko@gmail.com>
2017-01-06 20:30:11 +0800

    Dl tar of nginx

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -22,13 +22,18 @@ cd $temp_dir
 echo "Temp dir: $temp_dir"
 
 PCRE_VERSION=${PCRE_VERSION-8.21}
+NGINX_VERSION=${NGINX_VERSION-1.5.7}
+
 pcre_tarball_url=http://ncu.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
+nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+
+echo "Downloading $nginx_tarball_url"
+curl -L $nginx_tarball_url | tar xzv
 
 echo "Downloading $pcre_tarball_url"
-(curl -L $pcre_tarball_url | tar xvj )
+(curl -L $pcre_tarball_url | tar xvj -C ./nginx)
 
 # Main dependencies
-git clone https://github.com/nginx/nginx --depth=1;
 git clone https://github.com/konstruxi/form-input-nginx-module --depth=1;
 git clone https://github.com/konstruxi/ngx_postgres --depth=1;
 git clone https://github.com/konstruxi/mustache-nginx-module --depth=1;
